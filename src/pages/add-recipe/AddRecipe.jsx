d19877a4f2701d87a5bf1f5339263fb33b5c400a
tuan <rickytuan1991@gmail.com>
2022-12-13 01:23:36 +0200

    add recipe

--- src/pages/add-recipe/AddRecipe.jsx
+++ src/pages/add-recipe/AddRecipe.jsx
@@ -1,12 +1,139 @@
-import React from 'react'
-import style from './addrecipe.module.css'
+import React, { useState } from "react";
+import style from "./addrecipe.module.css";
 
 export default function AddRecipe() {
+  const [newRecipes, setNewRecipes] = useState({
+    name: "",
+    author: "",
+    country: "",
+    description: "",
+    ingredients: [
+      {
+        name: "",
+        quantity: "",
+        unit: "",
+      },
+      {
+        name: "",
+        quantity: "",
+        unit: "",
+      },
+    ],
+    preparation_time: "",
+    cooking_time: "",
+    servings: "",
+    directions: ["", "", ""],
+    nutrition: {
+      calories: 900,
+      fat: 60,
+      carbohydrates: 60,
+      protein: 30,
+    },
+  });
+  const addMoreIngredients = () => {
+    let temp = [...newRecipes.ingredients];
+    temp.push({
+      name: "",
+      quantity: "",
+      unit: "",
+    });
+    setNewRecipes({ ...newRecipes, ingredients: temp });
+  };
+  const addMoreDirection = () => {
+    let temp = [...newRecipes.directions];
+    temp.push("");
+    setNewRecipes({ ...newRecipes, directions: temp });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const render = () => {
+    let render = [];
+    for (let i in newRecipes) {
+      if (typeof newRecipes[i] === "string") {
+        render.push(
+          <div className="w-full flex justify-between">
+            <label htmlFor={`${i}`} className="">
+              {i}
+            </label>
+            <input type="text" id={i} name={i} value={newRecipes[i]} className="w-3/4" />
+          </div>
+        );
+      }
+      if (i === "nutrition") {
+        render.push(
+          <div className="w-full flex">
+            <p className="w">{i}</p>
+            <div className="w-2/3 flex  items-center gap-5 flex-wrap">
+              <label htmlFor={newRecipes[i]["calories"]}> calories</label>
+              <input type="number" id={newRecipes[i]["calories"]} name={newRecipes[i]["calories"]} value={newRecipes[i]["calories"]} />
+              
+            </div>
+          </div>
+        );
+      }
+      if (i === "ingredients") {
+        render.push(
+          <div className="w-full flex flex-wrap gap-5 justify-between">
+            <p className="">{i}</p>
+            <div className="w-2/3 flex  items-center gap-5 flex-wrap ">
+              {newRecipes[i]?.map((item, index) => {
+                return (
+                  <div className="w-full flex  items-center gap-5 " key={index}>
+                    <label htmlFor={`${item["name"]}`}>name</label>
+                    <input type="text" name={`${item["name"]}`} id={`${item["name"]}`} value={`${item["name"]}`} className="w-2/5" />
+                    <label htmlFor={`${item["quantity"]}`}>quantity</label>
+                    <input type="text" name={`${item["quantity"]}`} id={`${item["quantity"]}`} value={`${item["quantity"]}`} className="w-2/5" />
+                    <label htmlFor={`${item["unit"]}`}>unit</label>
+                    <input type="text" name={`${item["unit"]}`} id={`${item["unit"]}`} value={`${item["unit"]}`} className="w-2/5" />
+                  </div>
+                );
+              })}
+              <div className="w-full flex justify-end">
+                <button className="button px-4">more ingredients +</button>
+              </div>
+            </div>
+          </div>
+        );
+      }
+      if (i === "directions") {
+        render.push(
+          <div className="w-full flex flex-wrap gap-5 justify-between">
+            <p className="">{i}</p>
+            <div className="w-2/3 flex  items-center gap-5 flex-wrap ">
+              {newRecipes[i].map((item, index) => {
+                return (
+                  <div className="w-full justify-end flex gap-5" key={index}>
+                    <label htmlFor={`step${index}`}>step {index + 1}</label>
+                    <input type="text" name={`step${index}`} id={`step${index}`} className="w-2/3" value={item} />
+                  </div>
+                );
+              })}
+            </div>
+
+            <div className="w-full flex justify-end">
+              <button className="button px-4">more step +</button>
+            </div>
+          </div>
+        );
+      }
+    }
+    return render;
+  };
   return (
-    <div className='main'>
-        <div className={style['page']}>
-            <h1>add recipe</h1>
-        </div>
+    <div className="main">
+      <div className="h-full">
+        <h1 className="text-center text-6xl font-semibold pb-10">add recipes</h1>
+        <form
+          action=""
+          className="w-3/5 m-auto flex flex-wrap gap-5 "
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          {render()}
+        </form>
+      </div>
     </div>
-  )
+  );
 }
--- src/popup/FoodDetails.jsx
+++ src/popup/FoodDetails.jsx
@@ -2,23 +2,84 @@ import React, { useState } from "react";
 import { GET_IN, GET_OUT } from "../service/ultilities";
 import style from "./fooddetails.module.css";
 export default function FoodDetails({ popup, setPopup, detail }) {
-  const { name, author, country, description, image, ingredients, directions, nutrition, servings, cooking_time, preparation_time } = detail;
+  const { name, author, country, description, image, ingredients, directions, servings, cooking_time, preparation_time } = detail;
   const [show, setShow] = useState(GET_IN);
-  const render = () => {
-    let render = [];
-  };
+
   return popup ? (
-    <div className={`absolute z-40 w-full h-full ${show}`}>
+    <div className={`absolute z-40 w-full h-full ${show} flex flex-col items-center `}>
       <div className={style["pop_up"]}>
         <div className="w-full h-full flex flex-wrap justify-center relative">
-          <h1 className="w-full">
-            {name} by {author}
+          <h1 className="w-full label text-center py-10">
+            <span className="text-red-300">{name} -</span>
+            by <span className="text-red-300">- {author}</span>
           </h1>
-          <form action="" className="flex flex-wrap ">
-            <label htmlFor="country" className="w-1/4">
-              country
-            </label>
-            <input type="text" value={country} readOnly className="w-3/4" id="country" name="country" />
+          <form action="" className="flex flex-col gap-5 mb-5 w-full m-auto justify-center items-center ">
+            <div className="gap-3 flex m-auto justify-between w-2/3">
+              <h3 htmlFor="country" className="label">
+                country:
+              </h3>
+              <p className="">{country}</p>
+            </div>
+            <div className="gap-3 flex m-auto justify-between w-2/3">
+              <h3 htmlFor="country" className="label">
+                cooking time:
+              </h3>
+              <p className="">{cooking_time} mins</p>
+            </div>
+            <div className="gap-3 flex m-auto justify-between w-2/3">
+              <h3 htmlFor="country" className="label">
+                preparation time:
+              </h3>
+              <p className="">{preparation_time} mins</p>
+            </div>
+            <div className="gap-3 flex m-auto justify-between w-2/3">
+              <h3 htmlFor="country" className="label">
+                servings:
+              </h3>
+              <p className="">{servings} people</p>
+            </div>
+            <div className="gap-3 flex m-auto justify-between w-2/3">
+              <h3 htmlFor="country" className="label">
+                description:
+              </h3>
+              <p className="w-2/3 text-justify">{description} </p>
+            </div>
+            <div className="gap-3 flex m-auto justify-between w-2/3">
+              <h3 htmlFor="country" className="label">
+                ingredients:
+              </h3>
+              <table className="w-4/6 text-left">
+                <thead className="text-red-300">
+                  <tr>
+                    <th>name</th>
+                    <th>quantity</th>
+                    <th>unit</th>
+                  </tr>
+                </thead>
+                <tbody className="text-green-100 text-sm">
+                  {ingredients.map((item, index) => {
+                    return (
+                      <tr>
+                        <td>{item.name}</td>
+                        <td>{item.quantity}</td>
+                        <td>{item.unit}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+            <div className="gap-3 flex m-auto justify-between w-2/3">
+              <h3 htmlFor="country" className="label">
+                Cooking step:
+              </h3>
+              <div className="flex flex-col gap-2 w-2/3">
+                {directions.map((item, index) => {
+                  return <p className="text-sm">{`step ${index + 1}: ${item}`}</p>;
+                })}
+              </div>
+            </div>
+            <div className="gap-3 flex m-auto justify-between w-2/3"></div>
           </form>
           <div className="absolute right-4 top-4">
             <button
